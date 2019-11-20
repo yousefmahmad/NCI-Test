f715a39e9f31251485783eca5ca7f8c5a6c04cdd
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-11-19 22:20:32 -0500

    added get async

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-axios({
-  url: ''
-})
+const getData = async () => {
+  return await axios({
+    url: 'https://health.data.ny.gov/resource/gnzp-ekau.json'
+  })
+}
+
 
 
 
