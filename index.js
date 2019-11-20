24eda00247d115aa8e418622edd437938e1204b4
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-11-19 22:53:03 -0500

    getting stuck on the OAuth with the App token

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ const express = require('express');
 const axios = require('axios')
 const app = express();
 
-const port = process.env.PORT || 3000;
 
 // retrieves data from the API and fulfils promise with async and await
 const getData = async () => {
   return await axios({
-    url: 'https://health.data.ny.gov/resource/gnzp-ekau.json',
+    url: 'https://health.data.ny.gov/resource/gnzp-ekau.json', 
+    // Some sort of syntax error here that I'm getting stuck on
     {
-      headers: {
-        'X-App-Token': 'uc3bTDv5nIow7e035i9UpQdE8'
+      //   // it's an OAuth end point and so you need to add this header in your axios get with the app token
+        headers: {
+          'X-App-Token': 'uc3bTDv5nIow7e035i9UpQdE8'
+        }
       }
-    }
   })
 }
 
 // console log data to see data results retrieved
   console.log(getData())
 
+  app.listen(4040, () => console.log('working on port 4040'));
 
 
 
